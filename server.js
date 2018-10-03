4e7cebaeb4c262405fa3d7afff1ef414fa01a555
Victor Mwania <victormwnza@gmail.com>
2018-10-03 20:49:59 +0300

    created a response for when the input is unixtime

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const moment = require('moment')
 const monthString=['January','February','Month','April','May','June','July','August','September','October','November','December']
-
+const dayString =['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 // enable CORS 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200})); 
 
+const localDate = moment().format()
 
 app.use(express.static('public'));
 
@@ -19,22 +21,22 @@ app.get('/:timestamp', function (req, res) {
   const timeStamp= req.params.timestamp
   if (isNaN(timeStamp)) {
     const timestring=new Date(timeStamp)
-    if (typeof time=='number') {
-      res.json({
-        unix: timestring/1000,
-        utc: monthString[timestring.getMonth()]+' '+times.getDate()+', '+times.getFullYear()
-    })
-    } else {
-      res.json({
-        unix: null,
-        utc: null
-      })
-    }
-  } else {
-    const timstring1 = new Date(timeStamp*1000)
+    console.log(timestring.getTime()/1000)
+    const unixtt =  timestring.getTime()/1000;
+    unixtimeConvert
+    res.json({
+      unix:unixtt,
+      utc: moment(timestring).format("YYYY-MM-DD")
+  })
+  } 
+  // when the input is unix 
+  else {
+  //coverting unix to day, month year .....
+    const unixtime = new Date(timeStamp*1000)
+    unixtimeConvert(unixtime)
     res.json({
       unix: timeStamp,
-      utc: monthString[timstring1.getMonth()]+' '+timstring1.getDate()+', '+timstring1.getFullYear()
+      utc: this.utc
     })
   }
 })
@@ -42,3 +44,16 @@ app.get('/:timestamp', function (req, res) {
 var listener = app.listen(process.env.PORT || 8080 , function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+function  unixtimeConvert ( time) {
+  const date = time.getDate();
+  const month = monthString[time.getMonth()];
+  const year = time.getFullYear();
+  const day = dayString[time.getDay()];
+  const hours = time.getHours();
+  const minutes =  time.getMinutes();
+  const seconds =  time.getSeconds();
+
+  utc=  day+',' + ' ' + date+' '+ month+' ' + year +' ' + hours+':'+minutes+':'+seconds+' GMT' 
+  return utc;
+}
